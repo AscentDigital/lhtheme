8bb2c1d45dcc5c3456eacd2a3879239a0f27da10
Stephan Sutter <stephan.sutter2000@gmail.com>
2017-11-10 10:13:02 +0800

    Removed unnecessary imports in footer

--- footer.php
+++ footer.php
@@ -8,30 +8,19 @@
   </div>
 </footer>
 <!-- footer -->
-
-
-
-    <!-- Bootstrap core JavaScript
-      ================================================== -->
-      <!-- Placed at the end of the document so the pages load faster -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
-      <script src="./js/bootstrap.min.js"></script> 
-      <script src="./js/parallaxie.js"></script>  
-      <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-      <script src="./js/ie10-viewport-bug-workaround.js"></script>
-      <script>
-        jQuery(function( $ ){
-         $('.parallaxie').parallaxie();
-         $.fn.extend({
-          toggleText: function(a, b){
-            return this.text(this.text() == b ? a : b);
-          }
-        });
-         $('#mobile-menu').on('click', function(){
-          $('.nav-list-mobile').toggleClass('open');
-          $(this).toggleText('MENU', 'CLOSE');
-        });
-       });
-     </script>
-   </body>
-   </html>
+  <script>
+    jQuery(function( $ ){
+     $('.parallaxie').parallaxie();
+     $.fn.extend({
+      toggleText: function(a, b){
+        return this.text(this.text() == b ? a : b);
+      }
+    });
+     $('#mobile-menu').on('click', function(){
+      $('.nav-list-mobile').toggleClass('open');
+      $(this).toggleText('MENU', 'CLOSE');
+    });
+   });
+ </script>
+</body>
+</html>
